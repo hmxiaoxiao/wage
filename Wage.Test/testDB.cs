9ae922271be0c9f6a1e77c92b6cfbb3d3843a81b
hmxiaoxiao <hmxiaoxiao@gmail.com>
2013-10-22 17:00:47 +0800

    初始化项目 Signed-off-by: hmxiaoxiao <hmxiaoxiao@gmail.com>

---
+++ Wage.Test/testDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wage.Test
+{
+    /// <summary>
+    /// 测试数据库的库
+    /// </summary>
+    [TestClass]
+    public class testDB
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
